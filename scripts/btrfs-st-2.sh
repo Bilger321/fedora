40fb0ee62184ea8659752bd0514e48a9e63f5c98
Jacob Bilger <bilger.jacob@gmail.com>
2022-11-25 13:53:37 -0600

    Create btrfs-st-2.sh

---
+++ scripts/btrfs-st-2.sh
@@ -0,0 +1,12 @@
+umount /.snapshots
+rmdir /.snapshots
+snapper -c root create-config /
+btrfs subvolume delete /.snapshots
+mkdir /.snapshots
+systemctl daemon-reload
+mount -a
+snapper -c root set-config ALLOW_USERS=jbilger SYNC_ACL=yes
+chown -R :jbilger /.snapshots
+grub2-editenv - unset menu_auto_hide
+git clone https://github.com/Antynea/grub-btrfs.git /tmp/grub-btrfs
+make -C /tmp/grub-btrfs install
