856af7319018211322ea7ecb628bbe40dc96ad35
bilger321 <bilger.jacob@gmail.com>
2022-11-26 14:13:53 -0600

    Add forgotten patch file, rework application method

--- btrfs/scripts/btrfs-st-1.sh
+++ btrfs/scripts/btrfs-st-1.sh
@@ -4,5 +4,5 @@ chattr -R +C /var/lib/libvirt
 echo $'SUSE_BTRFS_SNAPSHOT_BOOTING="true"' >> /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
 systemctl disable install_stage_1.service
-systemctl enable /opt/fedora-main/btrfs-fed-37/scripts/install_stage_2.service
+systemctl enable /opt/fedora-main/btrfs/scripts/install_stage_2.service
 reboot
--- btrfs/scripts/btrfs-st-2.sh
+++ btrfs/scripts/btrfs-st-2.sh
@@ -11,7 +11,8 @@ snapper -c root set-config ALLOW_USERS=jbilger SYNC_ACL=yes
 chown -R :jbilger /.snapshots
 grub2-editenv - unset menu_auto_hide
 git clone https://github.com/Antynea/grub-btrfs.git /opt/grub-btrfs
+git -C /opt/grub-brtfs apply /opt/fedora-main/btrfs/scripts/btrfs.patch
 make -C /opt/grub-btrfs install
-systemctl disable /opt/fedora-main/btrfs-fed-37/scripts/install_stage_2.service
-systemctl enable /opt/fedora-main/btrfs-fed-37/scripts/install_stage_3.service
+systemctl disable /opt/fedora-main/btrfs/scripts/install_stage_2.service
+systemctl enable /opt/fedora-main/btrfs/scripts/install_stage_3.service
 reboot
